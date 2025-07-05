8a9faad0cd8a1fb89f110abc962470bcfb0a7b86
Hermann <hermann.inzillo@gmx.at>
2025-07-05 20:07:33 +0200

    handle cors

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 require('./server');
 const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  });
 
-  win.loadFile('index.html');
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    win.webContents.session.webRequest.onBeforeSendHeaders(
+        (details, callback) => {
+            callback({requestHeaders: {Origin: '*', ...details.requestHeaders}});
+        },
+    );
+
+    win.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+        callback({
+            responseHeaders: {
+                'Access-Control-Allow-Origin': ['*'],
+                ...details.responseHeaders,
+            },
+        });
+    });
+
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
+
